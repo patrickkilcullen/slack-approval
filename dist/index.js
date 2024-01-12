a199614396329fd3f39cb08bef27440ce96e7391
Patrick Kilcullen <patrick.kilcullen@hawkeyeinnovations.com>
2024-01-12 15:54:13 +0000

    Update formatting

--- dist/index.js
+++ dist/index.js
@@ -81,7 +81,7 @@ function run() {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                "text": `*Plan:*\n \`\`\` ${plan}\n\`\`\``,
+                                "text": `*Plan:*\n \`\`\`${plan}\n\`\`\``,
                             }
                         },
                         {
